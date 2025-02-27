d3b9f6e1cd9fd43ef290347d380d80c30c45c37e
jagruti21kadam <153177889+jagruti21kadam@users.noreply.github.com>
2025-02-27 17:45:47 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class EvenOdd
+{
+        public static void main(String args[]) 
+          {
+               int num=25;
+               if(num%2==0)
+                {
+                   System.out.println("Given number is even :");
+                 }
+               else
+                 {
+                     System.out.println("Given number is odd:");
+                  }
+          }
+}
